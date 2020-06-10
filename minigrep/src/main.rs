fbd6336d6f8a87808091821ed9391d9c5785dfea
Ben Windsor <benjaminwindsor@hotmail.co.uk>
2020-06-10 21:57:55 +0100

    Initial commit of project

---
+++ minigrep/src/main.rs
@@ -0,0 +1,49 @@
+use std::env; 
+use std::process;
+use std::fs;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {} in file {}", config.query, config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Error running minigrep: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)
+        .expect("Error loading file");
+    
+    println!("File contents:\n{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+}
+
+
